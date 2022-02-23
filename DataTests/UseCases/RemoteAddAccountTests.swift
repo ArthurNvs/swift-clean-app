7c8a4a3e6a89432a768c5ecb84bb8a7b02a241cb
Arthur Neves <arthurnvsm@gmail.com>
2022-02-23 11:21:54 -0300

    add new params to function expect

--- DataTests/UseCases/RemoteAddAccountTests.swift
+++ DataTests/UseCases/RemoteAddAccountTests.swift
@@ -49,13 +49,13 @@ extension RemoteAddAccountTests {
     return (sut, httpClientSpy)
   }
   
-  func expect(_ sut: RemoteAddAccount, completeWith expectedResult: Result<AccountModel, DomainError>, when action: () -> Void) {
+  func expect(_ sut: RemoteAddAccount, completeWith expectedResult: Result<AccountModel, DomainError>, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
     let exp = expectation(description: "waiting")
     sut.add(addAccountModel: makeAddAccountModel()) { receivedResult in
       switch (expectedResult, receivedResult) {
-      case (.failure(let expectedError), .failure(let receivedError)): XCTAssertEqual(expectedError, receivedError)
-      case (.success(let expectedAccount), .success(let receivedAccount)): XCTAssertEqual(expectedAccount, receivedAccount)
-      default: XCTFail("Expected \(expectedResult) received \(receivedResult) instead")
+      case (.failure(let expectedError), .failure(let receivedError)): XCTAssertEqual(expectedError, receivedError, file: file, line: line)
+      case (.success(let expectedAccount), .success(let receivedAccount)): XCTAssertEqual(expectedAccount, receivedAccount, file: file, line: line)
+      default: XCTFail("Expected \(expectedResult) received \(receivedResult) instead", file: file, line: line)
       }
       exp.fulfill()
     }
