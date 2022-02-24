2d9906746c51f7a4f4e00e6c72a45eb9260b3c6c
Arthur Neves <arthurnvsm@gmail.com>
2022-02-24 12:04:03 -0300

    ensure AlamofireAdapter calls Alamofire with correct verb

--- InfraTests/AlamofireAdapterTests.swift
+++ InfraTests/AlamofireAdapterTests.swift
@@ -10,7 +10,7 @@ class AlamofireAdapter {
   }
   
   func post(to url: URL) {
-    session.request(url).resume()
+    session.request(url, method: .post).resume()
   }
 }
 
@@ -25,6 +25,7 @@ class AlamofireAdapterTests: XCTestCase {
     let exp = expectation(description: "waiting")
     UrlProtocolStub.observeRequest { request in
       XCTAssertEqual(url, request.url)
+      XCTAssertEqual("POST", request.httpMethod)
       exp.fulfill()
     }
     wait(for: [exp], timeout: 1)
