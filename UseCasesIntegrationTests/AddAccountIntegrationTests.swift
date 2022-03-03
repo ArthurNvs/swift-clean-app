ad35e196f58335d9794b589d8e1bf45bd215bf23
Arthur Neves <arthurnvsm@gmail.com>
2022-03-03 18:54:13 -0300

    add  AddAccount integration tests

---
+++ UseCasesIntegrationTests/AddAccountIntegrationTests.swift
@@ -0,0 +1,27 @@
+//  Created by Arthur Neves on 03/03/22.
+
+import XCTest
+import Data
+import Infra
+import Domain
+
+class AddAccountIntegrationTests: XCTestCase {
+    func test_add_account() {
+      let alamofireAdapter = AlamofireAdapter()
+      let url = URL(string: "https://clean-node-api.herokuapp.com/api/signup")!
+      let sut = RemoteAddAccount(url: url, httpClient: alamofireAdapter)
+      let addAccountModel = AddAccountModel(name: "Arthur", email: "arthur.neves@gmail.com", password: "pswd", passwordConfirmation: "pswd")
+      let exp = expectation(description: "waiting")
+      sut.add(addAccountModel: addAccountModel) { result in
+        switch result {
+        case.failure: XCTFail("Expect succes but got \(result) instead.")
+        case.success(let account):
+          XCTAssertNotNil(account.id)
+          XCTAssertEqual(account.name, addAccountModel.name)
+          XCTAssertEqual(account.email, addAccountModel.email)
+        }
+        exp.fulfill()
+      }
+      wait(for: [exp], timeout: 15)
+    }
+}
