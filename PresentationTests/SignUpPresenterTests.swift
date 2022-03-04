8de2409410189a9e19e244bfbeb03f36cb500a72
Arthur Neves <arthurnvsm@gmail.com>
2022-03-04 07:59:01 -0300

    add presentation layer

---
+++ PresentationTests/SignUpPresenterTests.swift
@@ -0,0 +1,9 @@
+//  Created by Arthur Neves on 04/03/22.
+
+import XCTest
+
+class SignUpPresenterTests: XCTestCase {
+  func test_() {
+    
+  }
+}
--- UseCasesIntegrationTests/AddAccountIntegrationTests.swift
+++ UseCasesIntegrationTests/AddAccountIntegrationTests.swift
@@ -6,38 +6,38 @@ import Infra
 import Domain
 
 class AddAccountIntegrationTests: XCTestCase {
-    func test_add_account() {
-      let alamofireAdapter = AlamofireAdapter()
-      let url = URL(string: "https://clean-node-api.herokuapp.com/api/signup")!
-      let sut = RemoteAddAccount(url: url, httpClient: alamofireAdapter)
-      let addAccountModel = AddAccountModel(name: "Arthur", email: "arthur.neves@gmail.com", password: "pswd", passwordConfirmation: "pswd")
-      let exp = expectation(description: "waiting")
-      sut.add(addAccountModel: addAccountModel) { result in
-        switch result {
-        case.failure: XCTFail("Expect succes but got \(result) instead.")
-        case.success(let account):
-          XCTAssertNotNil(account.id)
-          XCTAssertEqual(account.name, addAccountModel.name)
-          XCTAssertEqual(account.email, addAccountModel.email)
-        }
-        exp.fulfill()
+  func test_add_account() {
+    let alamofireAdapter = AlamofireAdapter()
+    let url = URL(string: "https://clean-node-api.herokuapp.com/api/signup")!
+    let sut = RemoteAddAccount(url: url, httpClient: alamofireAdapter)
+    let addAccountModel = AddAccountModel(name: "Arthur", email: "arthur.neves@gmail.com", password: "pswd", passwordConfirmation: "pswd")
+    let exp = expectation(description: "waiting")
+    sut.add(addAccountModel: addAccountModel) { result in
+      switch result {
+      case.failure: XCTFail("Expect succes but got \(result) instead.")
+      case.success(let account):
+        XCTAssertNotNil(account.id)
+        XCTAssertEqual(account.name, addAccountModel.name)
+        XCTAssertEqual(account.email, addAccountModel.email)
       }
-      wait(for: [exp], timeout: 15)
+      exp.fulfill()
     }
+    wait(for: [exp], timeout: 15)
+  }
   
-//  func test_add_account_failure() {
-//    let alamofireAdapter = AlamofireAdapter()
-//    let url = URL(string: "https://clean-node-api.herokuapp.com/api/signup")!
-//    let sut = RemoteAddAccount(url: url, httpClient: alamofireAdapter)
-//    let addAccountModel = AddAccountModel(name: "Arthur", email: "arthur.neves@gmail.com", password: "pswd", passwordConfirmation: "wrong_pswd")
-//    let exp = expectation(description: "waiting")
-//    sut.add(addAccountModel: addAccountModel) { result in
-//      switch result {
-//      case.failure(let error): XCTAssertEqual(error, .unexpected)
-//      case.success: XCTFail("Expect error but got \(result) instead.")
-//      }
-//      exp.fulfill()
-//    }
-//    wait(for: [exp], timeout: 15)
-//  }
+  //  func test_add_account_failure() {
+  //    let alamofireAdapter = AlamofireAdapter()
+  //    let url = URL(string: "https://clean-node-api.herokuapp.com/api/signup")!
+  //    let sut = RemoteAddAccount(url: url, httpClient: alamofireAdapter)
+  //    let addAccountModel = AddAccountModel(name: "Arthur", email: "arthur.neves@gmail.com", password: "pswd", passwordConfirmation: "wrong_pswd")
+  //    let exp = expectation(description: "waiting")
+  //    sut.add(addAccountModel: addAccountModel) { result in
+  //      switch result {
+  //      case.failure(let error): XCTAssertEqual(error, .unexpected)
+  //      case.success: XCTFail("Expect error but got \(result) instead.")
+  //      }
+  //      exp.fulfill()
+  //    }
+  //    wait(for: [exp], timeout: 15)
+  //  }
 }
