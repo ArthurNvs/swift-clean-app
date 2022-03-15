8c3e0a5bfe79417fb3ce678985571fa873bad8d0
Arthur Neves <arthurnvsm@gmail.com>
2022-03-14 22:31:45 -0300

    add Storyboarded protocol to simplify ViewController creation

---
+++ UI/Helpers/UIViewControllerExtensions.swift
@@ -0,0 +1,16 @@
+//  Created by Arthur Neves on 14/03/22.
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+  func hideKeyboardOnTap() {
+    let gesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+    gesture.cancelsTouchesInView = false
+    view.addGestureRecognizer(gesture)
+  }
+  
+  @objc private func hideKeyboard() {
+    view.endEditing(true)
+  }
+}
