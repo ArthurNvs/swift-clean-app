20d87d60a68a900c115e003bbb1b63761ff7fb8e
Arthur Neves <arthurnvsm@gmail.com>
2022-03-16 21:34:34 -0300

    add Main target

---
+++ Main/SceneDelegate.swift
@@ -0,0 +1,13 @@
+//  Created by Arthur Neves on 16/03/22.
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+  var window: UIWindow?
+  
+  func scene(_ scene: UIScene,
+             willConnectTo session: UISceneSession,
+             options connectionOptions: UIScene.ConnectionOptions) {
+    guard let _ = (scene as? UIWindowScene) else { return }
+  }
+}
