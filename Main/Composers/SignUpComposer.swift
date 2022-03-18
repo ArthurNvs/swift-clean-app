c462452e7e84b3202250bb6e55d6a1b84093e56e
Arthur Neves <arthurnvsm@gmail.com>
2022-03-17 21:27:22 -0300

    remove ControllerFactory and move to Compose

--- Main/Composers/SignUpComposer.swift
+++ Main/Composers/SignUpComposer.swift
@@ -1,11 +1,17 @@
 //  Created by Arthur Neves on 17/03/22.
 
 import Foundation
-import Domain
 import UI
+import Validation
+import Domain
+import Presentation
 
 public final class SignUpComposer {
   public static func composeControllerWith(_ addAccount: AddAccount) -> SignUpViewController {
-    return ControllerFactory.makeSignUpViewController(addAccount: addAccount)
+    let controller = SignUpViewController.instantiate()
+    let emailValidatorAdapter = EmailValidatorAdapter()
+    let presenter = SignUpPresenter(alertView: WeakVarProxy(controller), emailValidator: emailValidatorAdapter, addAccount: addAccount, loadingView: WeakVarProxy(controller))
+    controller.signUp = presenter.signUp
+    return controller
   }
 }
