9a18b30acd20c53795aa432243daae7d2782c8f9
Arthur Neves <arthurnvsm@gmail.com>
2022-03-19 11:26:52 -0300

    move threading logic to the decorator class

--- Main/Factories/UseCaseFactory.swift
+++ Main/Factories/UseCaseFactory.swift
@@ -14,6 +14,32 @@ final class UseCaseFactory {
   }
   
   static func makeRemoteAddAccount() -> AddAccount {
-    return RemoteAddAccount(url: makeUrl(path: "signup"), httpClient: httpClient)
+    let remoteAddAccount = RemoteAddAccount(url: makeUrl(path: "signup"), httpClient: httpClient)
+    // Calling the use case within the decorator
+    return MainQueueDispatchDecorator(remoteAddAccount)
+  }
+}
+
+// Implementing a decorator
+public final class MainQueueDispatchDecorator<T> {
+  private let instance: T
+  
+  public init(_ instance: T) {
+    self.instance = instance
+  }
+  
+  func dispatch(completion: @escaping () -> Void) {
+    // ensure decorator execute in Main Thread only if its not in Main Thread
+    guard Thread.isMainThread else { return DispatchQueue.main.async(execute: completion) }
+    completion()
+  }
+}
+
+extension MainQueueDispatchDecorator: AddAccount where T: AddAccount {
+  public func add(addAccountModel: AddAccountModel, completion: @escaping (Result<AccountModel, DomainError>) -> Void) {
+    instance.add(addAccountModel: addAccountModel) { [weak self] result in
+      // ensure the useCase is executed in Main Thread
+      self?.dispatch { completion(result) }
+    }
   }
 }
--- MainTests/Composers/SignUpComposerTest.swift
+++ MainTests/Composers/SignUpComposerTest.swift
@@ -6,15 +6,22 @@ import UI
 
 class SignUpComposerTest: XCTestCase {
   func test_background_request_should_complete_on_main_thread() {
-    let (sut, _) = makeSut()
+    let (sut, addAccountSpy) = makeSut()
     sut.loadViewIfNeeded()
+    sut.signUp?(makeSignUpViewModel())
+    let exp = expectation(description: "waiting")
+    DispatchQueue.global().async {
+      addAccountSpy.completeWithError(.unexpected)
+      exp.fulfill()
+    }
+    wait(for: [exp], timeout: 1)
   }
 }
 
 extension SignUpComposerTest {
   func makeSut(file: StaticString = #filePath, line: UInt = #line) -> (sut: SignUpViewController, addAccountSpy: AddAccountSpy) {
     let addAccountSpy = AddAccountSpy()
-    let sut = SignUpComposer.composeControllerWith(AddAccountSpy())
+    let sut = SignUpComposer.composeControllerWith(MainQueueDispatchDecorator(addAccountSpy))
     checkMemoryLeak(for: sut, file: file, line: line)
     checkMemoryLeak(for: addAccountSpy, file: file, line: line)
     return (sut, addAccountSpy)
