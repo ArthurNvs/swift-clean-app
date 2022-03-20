7cd9ab3b4ed7064c0304ec07effdb7ee056be0a5
Arthur Neves <arthurnvsm@gmail.com>
2022-03-20 12:39:08 -0300

    change remote API URL

--- UseCasesIntegrationTests/AddAccountIntegrationTests.swift
+++ UseCasesIntegrationTests/AddAccountIntegrationTests.swift
@@ -8,17 +8,15 @@ import Domain
 class AddAccountIntegrationTests: XCTestCase {
   func test_add_account() {
     let alamofireAdapter = AlamofireAdapter()
-    let url = URL(string: "http://localhost:5050/api/signup")!
+    let url = URL(string: "https://fordevs.herokuapp.com/api/signup")!
     let sut = RemoteAddAccount(url: url, httpClient: alamofireAdapter)
-    let addAccountModel = AddAccountModel(name: "Arthur", email: "arthur.neves@gmail.com", password: "pswd", passwordConfirmation: "pswd")
+    let addAccountModel = AddAccountModel(name: "Arthur", email: "arthur.nevs@gmail.com", password: "pswd", passwordConfirmation: "pswd")
     let exp = expectation(description: "waiting")
     sut.add(addAccountModel: addAccountModel) { result in
       switch result {
       case.failure: XCTFail("Expect succes but got \(result) instead.")
       case.success(let account):
-        XCTAssertNotNil(account.id)
-        XCTAssertEqual(account.name, addAccountModel.name)
-        XCTAssertEqual(account.email, addAccountModel.email)
+        XCTAssertNotNil(account.accessToken)
       }
       exp.fulfill()
     }
