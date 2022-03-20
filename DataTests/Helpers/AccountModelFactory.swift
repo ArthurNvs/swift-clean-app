679f2cacba859fe6a232d87d844d18b6f0927e0e
Arthur Neves <arthurnvsm@gmail.com>
2022-03-20 12:28:52 -0300

    change AccountModel to have only an accessToken

--- DataTests/Helpers/AccountModelFactory.swift
+++ DataTests/Helpers/AccountModelFactory.swift
@@ -4,7 +4,7 @@ import Foundation
 import Domain
 
 func makeAccountModel() -> AccountModel {
-  AccountModel(id: "any_id", name: "any_name", email: "any_email", password: "any_pswd")
+  AccountModel(accessToken: "any_token")
 }
 
 func makeAddAccountModel() -> AddAccountModel {
