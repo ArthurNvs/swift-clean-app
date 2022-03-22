34ec0fb921bf053f5d3ee94db508f921bd9699cd
Arthur Neves <arthurnvsm@gmail.com>
2022-03-22 08:26:00 -0300

    add Authentication use case

---
+++ Domain/UseCases/Authentication.swift
@@ -0,0 +1,18 @@
+//  Created by Arthur Neves on 22/03/22.
+
+import Foundation
+
+public protocol Authentication {
+  typealias Result = Swift.Result<AccountModel, DomainError>
+  func add(authenticationModel: AuthenticationModel, completion: @escaping (Result) -> Void)
+}
+
+public struct AuthenticationModel: Model {
+  public var email: String
+  public var password: String
+  
+  public init(email: String, password: String) {
+    self.email = email
+    self.password = password
+  }
+}
