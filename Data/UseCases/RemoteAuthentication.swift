c0d3a2bd3bdeba77a329cd1f62c3c493cb0af38d
Arthur Neves <arthurnvsm@gmail.com>
2022-03-23 20:45:47 -0300

    make RemoteAuthentication conforms to Authentication protocol

--- Data/UseCases/RemoteAuthentication.swift
+++ Data/UseCases/RemoteAuthentication.swift
@@ -3,7 +3,7 @@
 import Foundation
 import Domain
 
-public final class RemoteAuthentication {
+public final class RemoteAuthentication: Authentication {
   private let url: URL
   private let httpClient: HttpPostClient
   
