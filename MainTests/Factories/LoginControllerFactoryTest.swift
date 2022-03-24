5f3e8269b44cd68cab6fc091f51610fb4d223258
Arthur Neves <arthurnvsm@gmail.com>
2022-03-24 18:50:14 -0300

    ensure RemoteAuthentication completes on main queue

--- MainTests/Factories/LoginControllerFactoryTest.swift
+++ MainTests/Factories/LoginControllerFactoryTest.swift
@@ -6,6 +6,18 @@ import UI
 import Validation
 
 class LoginControllerFactoryTest: XCTestCase {
+  func test_background_request_should_complete_on_main_thread() {
+    let (sut, authenticationSpy) = makeSut()
+    sut.loadViewIfNeeded()
+    sut.login?(makeLoginViewModel())
+    let exp = expectation(description: "waiting")
+    DispatchQueue.global().async {
+      authenticationSpy.completeWithError(.unexpected)
+      exp.fulfill()
+    }
+    wait(for: [exp], timeout: 1)
+  }
+  
   func test_login_compose_with_correct_validations() {
     let validations = makeLoginValidations()
     XCTAssertEqual(validations[0] as! RequiredFieldValidation, RequiredFieldValidation(fieldName: "email", fieldLabel: "Email"))
@@ -17,7 +29,7 @@ class LoginControllerFactoryTest: XCTestCase {
 extension LoginControllerFactoryTest {
   func makeSut(file: StaticString = #filePath, line: UInt = #line) -> (sut: LoginViewController, authenticationSpy: AuthenticationSpy) {
     let authenticationSpy = AuthenticationSpy()
-    let sut = makeLoginController(authentication: authenticationSpy)
+    let sut = makeLoginController(authentication: MainQueueDispatchDecorator(authenticationSpy))
     checkMemoryLeak(for: sut, file: file, line: line)
     checkMemoryLeak(for: authenticationSpy, file: file, line: line)
     return (sut, authenticationSpy)
