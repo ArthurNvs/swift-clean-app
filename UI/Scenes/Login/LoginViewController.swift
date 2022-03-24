9b7bc27de8c65f2dc8849128e31dff779dd04c92
Arthur Neves <arthurnvsm@gmail.com>
2022-03-24 08:24:48 -0300

    ensure LoginViewController starts with hidden Loading

---
+++ UI/Scenes/Login/LoginViewController.swift
@@ -0,0 +1,33 @@
+//  Created by Arthur Neves on 08/03/22.
+
+import Foundation
+import UIKit
+import Presentation
+
+public final class LoginViewController: UIViewController, Storyboarded {
+  @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
+  @IBOutlet weak var loginButton: UIButton!
+  
+  public override func viewDidLoad() {
+    super.viewDidLoad()
+    configure()
+  }
+  
+  private func configure() {
+    title = "4Dev"
+    loginButton?.layer.cornerRadius = 5
+    hideKeyboardOnTap()
+  }
+}
+
+//extension LoginViewController: LoadingView {
+//  public func display(viewModel: LoadingViewModel) {
+//    if viewModel.isLoading {
+//      view.isUserInteractionEnabled = false
+//      loadingIndicator?.startAnimating()
+//    } else {
+//      view.isUserInteractionEnabled = true
+//      loadingIndicator?.stopAnimating()
+//    }
+//  }
+//}
---
+++ UITests/Scenes/Login/LoginViewControllerTests.swift
@@ -0,0 +1,20 @@
+//  Created by Arthur Neves on 08/03/22.
+
+import XCTest
+import UIKit
+import Presentation
+@testable import UI
+
+class LoginViewControllerTests: XCTestCase {
+  func test_loading_is_hidden_on_start() {
+    XCTAssertEqual(makeSut().loadingIndicator?.isAnimating, false)
+  }
+}
+
+extension LoginViewControllerTests {
+  func makeSut() -> LoginViewController {
+    let sut = LoginViewController.instantiate()
+    sut.loadViewIfNeeded()
+    return sut
+  }
+}
