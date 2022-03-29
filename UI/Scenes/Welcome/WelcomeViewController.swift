c290172688324736f55cc1a60e32d240ce75df34
Arthur Neves <arthurnvsm@gmail.com>
2022-03-29 08:08:51 -0300

    ensure WelcomeViewController calls signUp on button tapped

--- UI/Scenes/Welcome/WelcomeViewController.swift
+++ UI/Scenes/Welcome/WelcomeViewController.swift
@@ -8,6 +8,7 @@ public final class WelcomeViewController: UIViewController, Storyboarded {
   @IBOutlet weak var signUpButton: UIButton!
   
   public var login: (() -> Void)?
+  public var signUp: (() -> Void)?
   
   public override func viewDidLoad() {
     super.viewDidLoad()
@@ -18,10 +19,15 @@ public final class WelcomeViewController: UIViewController, Storyboarded {
     title = "4Dev"
     loginButton?.layer.cornerRadius = 5
     loginButton?.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
-    hideKeyboardOnTap()
+    signUpButton?.layer.cornerRadius = 5
+    signUpButton?.addTarget(self, action: #selector(signUpButtonTapped), for: .touchUpInside)
   }
   
   @objc private func loginButtonTapped() {
     login?()
+  }
+  
+  @objc private func signUpButtonTapped() {
+    signUp?()
   }
 }
