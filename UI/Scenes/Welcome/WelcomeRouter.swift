3b2435cdd4dbb5ea4da17e2612091c9d8ade571b
Arthur Neves <arthurnvsm@gmail.com>
2022-03-30 20:54:52 -0300

    move WelcomeRouter to its own file

---
+++ UI/Scenes/Welcome/WelcomeRouter.swift
@@ -0,0 +1,25 @@
+//  Created by Arthur Neves on 30/03/22.
+
+import Foundation
+
+public final class WelcomeRouter {
+  private let nav: NavigationController
+  private let loginFactory: () -> LoginViewController
+  private let signUpFactory: () -> SignUpViewController
+  
+  init(nav: NavigationController,
+       loginFactory: @escaping () -> LoginViewController,
+       signUpFactory: @escaping () -> SignUpViewController) {
+    self.nav = nav
+    self.loginFactory = loginFactory
+    self.signUpFactory = signUpFactory
+  }
+  
+  public func goToLogin() {
+    nav.pushViewController(loginFactory())
+  }
+  
+  public func goToSignUp() {
+    nav.pushViewController(signUpFactory())
+  }
+}
--- UITests/Scenes/Welcome/WelcomeRouterTests.swift
+++ UITests/Scenes/Welcome/WelcomeRouterTests.swift
@@ -4,28 +4,6 @@ import XCTest
 import UIKit
 import UI
 
-public final class WelcomeRouter {
-  private let nav: NavigationController
-  private let loginFactory: () -> LoginViewController
-  private let signUpFactory: () -> SignUpViewController
-  
-  init(nav: NavigationController,
-       loginFactory: @escaping () -> LoginViewController,
-       signUpFactory: @escaping () -> SignUpViewController) {
-    self.nav = nav
-    self.loginFactory = loginFactory
-    self.signUpFactory = signUpFactory
-  }
-  
-  public func goToLogin() {
-    nav.pushViewController(loginFactory())
-  }
-  
-  public func goToSignUp() {
-    nav.pushViewController(signUpFactory())
-  }
-}
-
 class WelcomeRouterTests: XCTestCase {
   func test_goToLogin_calls_nav_with_correct_view_controller() {
     let (sut, nav) = makeSut()
